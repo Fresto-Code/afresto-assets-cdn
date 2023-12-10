cf9a3c827d1f82f1f02c6f9a1a181917470ccf22
Chabib Nurozak <chabibnurozak@gmail.com>
2023-12-10 21:54:03 +0700

    update voice

--- js/main.js
+++ js/main.js
@@ -4,7 +4,10 @@ $(document)
   })
   .on("pjax:complete pjax:end", function () {
     $(".pjax-loader").hide();
+    
     try {
+      $(".drawer-overlay").remove();
+      $('[data-control="select2"].select2-hidden-accessible').select2("destroy")
       KTComponents.init();
     } catch (e) {}
   });
