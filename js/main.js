1fce7877116d1bf00a5c337bbf7a39d2acf18f6f
Chabib Nurozak <chabibnurozak@gmail.com>
2024-07-04 09:08:00 +0700

    main

--- js/main.js
+++ js/main.js
@@ -4,12 +4,13 @@ $(document)
   })
   .on("pjax:complete pjax:end", function () {
     $(".pjax-loader").hide();
-    
+
     try {
       $('[data-control="select2"].select2-hidden-accessible')
-      .removeClass('select2-hidden-accessible')
-      .next().remove();
-      $('[data-control="select2"]').select2()
+        .removeClass("select2-hidden-accessible")
+        .next()
+        .remove();
+      $('[data-control="select2"]').select2();
     } catch (e) {}
 
     try {
@@ -18,4 +19,29 @@ $(document)
     } catch (e) {}
   });
 
-  $("[data-persistent-size=1]").height($("[data-persistent-size=1]").height())
+$("[data-persistent-size=1]").height($("[data-persistent-size=1]").height());
+
+function initLoader(text) {
+  const loadingEl = document.createElement("div");
+  document.body.prepend(loadingEl);
+  loadingEl.classList.add("page-loader");
+  loadingEl.classList.add("flex-column");
+  loadingEl.classList.add("bg-dark");
+  loadingEl.classList.add("bg-opacity-25");
+  loadingEl.innerHTML = `
+            <div class="align-items-center card d-flex flex-column p-5">
+                <span class="spinner-border text-primary" role="status"></span>
+                <span class="text-gray-800 fs-6 fw-semibold mt-5">${text}</span>
+            </div>
+        
+    `;
+
+  // Show page loading
+  KTApp.showPageLoading();
+  return loadingEl;
+}
+
+function removeLoader(el) {
+  el.remove();
+  KTApp.hidePageLoading();
+}
